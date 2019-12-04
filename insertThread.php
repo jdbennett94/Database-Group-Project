c02d7148d03d23906d21d3b3f364caac138d27c5
alyssaschilke <arschilke@loyola.edu>
2019-12-04 02:25:08 -0500

    Json coversion started for insert Thread

--- insertThread.php
+++ insertThread.php
@@ -1,10 +1,27 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>insertThread.php</title>
-  </head>
-  <body>
+<?php
 
-  </body>
-</html>
+#The object should have the following keys (and appropriate values):body, name (of user), datePosted (you can retrieve it in your PHP)
+
+
+
+$thread->body = $_POST["body"];
+$thread->name = $_POST["user"];
+$thread->datePosted = date("Y-m-d");
+
+$threadJSON = json_encode([$thread]);
+
+
+#$tagsJSON = json_encode([$_POST["tag1"],$_POST["tag2"],$_POST["tag3"],$_POST["tag4"],$_POST["tag5"]]);
+
+
+
+
+
+echo $myJSON;
+
+
+
+
+
+
+?>
